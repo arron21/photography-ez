2c82e7f8923b3a750127f3704665f08d14c4dd5d
arron <arronmccrory@gmail.com>
2024-12-27 09:57:32 -0500

    updated index message

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,12 +8,11 @@ export default function Home() {
     <>
       <div class="">
 
-        <GalleryInfo heading={SiteName ?? "Photography Portfolio"} copy={`Thanks for checking out my photography. My photography is often what catches my eye. The juxtaposition of
+        <GalleryInfo heading={SiteName ?? "Photography Portfolio"} copy={`Thanks for checking out my photography. The juxtaposition of
             nature and human made objects is what I find interesting. We build
             tall sky scrapers so we can see beyond the trees. We build
             televisions so we can watch birds inside of a box. I try to capture
-            the balance between our human existence and the existence of Earth
-            itself.`} />
+            the balance between our human existence and Earth.`} />
       
       </div>
     </>
